2c08257c34c753dd28b653f962a192ca9f8b8997
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2026-09-05 19:58:23 -0600

    make title screen menu use the regular font vram

--- mappings/sprite/obj0F.asm
+++ mappings/sprite/obj0F.asm
@@ -1,27 +1,41 @@
-Map_obj0F:	mappingsTable
+Map_f962: mappingsTable
 	mappingsTableEntry.w	Map_obj0F_0006
-	mappingsTableEntry.w	Map_obj0F_0048
+	mappingsTableEntry.w	Map_obj0F_0006_Clone
 
 Map_obj0F_0006:	spriteHeader
-	spritePiece	-$2C, -$C, 1, 1, $402, 0, 0, 3, 0
-	spritePiece	-$1C, -$C, 4, 1, $403, 0, 0, 3, 0
-	spritePiece	4, -$C, 2, 1, $407, 0, 0, 3, 0
-	spritePiece	-$2C, 4, 1, 1, $409, 0, 0, 0, 0
-	spritePiece	-$1C, 4, 4, 1, $403, 0, 0, 0, 0
-	spritePiece	4, 4, 2, 1, $407, 0, 0, 0, 0
-	spritePiece	$1C, 4, 2, 1, $40A, 0, 0, 0, 0
-	spritePiece	-$44, -$C, 2, 1, $3FA, 0, 0, 3, 0
+ spritePiece -$3D, -$C, 1, 1, $A, 0, 0, 3, 0
+ spritePiece -$2C, -$C, 2, 1, $20, 0, 0, 3, 0
+ spritePiece -$1C, -$C, 1, 1, $E, 0, 0, 3, 0
+ spritePiece -$14, -$C, 1, 1, $1F, 0, 0, 3, 0
+ spritePiece -$C, -$C, 1, 1, $21, 0, 0, 3, 0
+ spritePiece 4, -$C, 1, 1, $14, 0, 0, 3, 0
+ spritePiece $C, -$C, 1, 1, $E, 0, 0, 3, 0
+ spritePiece $14, -$C, 1, 1, $1A, 0, 0, 3, 0
+ spritePiece $1C, -$C, 1, 1, $12, 0, 0, 3, 0
+ spritePiece -$2B, 4, 2, 1, $1C, 0, 0, 0, 0
+ spritePiece -$1B, 4, 1, 1, $21, 0, 0, 0, 0
+ spritePiece -$12, 4, 1, 1, $16, 0, 0, 0, 0
+ spritePiece -$A, 4, 1, 1, $1C, 0, 0, 0, 0
+ spritePiece -1, 4, 1, 1, $1B, 0, 0, 0, 0
+ spritePiece 7, 4, 1, 1, $20, 0, 0, 0, 0
 Map_obj0F_0006_End
 
-Map_obj0F_0048:	spriteHeader
-	spritePiece	-$2C, -$C, 1, 1, $402, 0, 0, 0, 0
-	spritePiece	-$1C, -$C, 4, 1, $403, 0, 0, 0, 0
-	spritePiece	4, -$C, 2, 1, $407, 0, 0, 0, 0
-	spritePiece	-$2C, 4, 1, 1, $409, 0, 0, 3, 0
-	spritePiece	-$1C, 4, 4, 1, $403, 0, 0, 3, 0
-	spritePiece	4, 4, 2, 1, $407, 0, 0, 3, 0
-	spritePiece	$1C, 4, 2, 1, $40A, 0, 0, 3, 0
-	spritePiece	-$44, 4, 2, 1, $3FA, 0, 0, 3, 0
-Map_obj0F_0048_End
+Map_obj0F_0006_Clone:	spriteHeader
+ spritePiece -$3D, 4, 1, 1, $A, 0, 0, 3, 0
+ spritePiece -$2C, -$C, 2, 1, $20, 0, 0, 0, 0
+ spritePiece -$1C, -$C, 1, 1, $E, 0, 0, 0, 0
+ spritePiece -$14, -$C, 1, 1, $1F, 0, 0, 0, 0
+ spritePiece -$C, -$C, 1, 1, $21, 0, 0, 0, 0
+ spritePiece 4, -$C, 1, 1, $14, 0, 0, 0, 0
+ spritePiece $C, -$C, 1, 1, $E, 0, 0, 0, 0
+ spritePiece $14, -$C, 1, 1, $1A, 0, 0, 0, 0
+ spritePiece $1C, -$C, 1, 1, $12, 0, 0, 0, 0
+ spritePiece -$2B, 4, 2, 1, $1C, 0, 0, 3, 0
+ spritePiece -$1B, 4, 1, 1, $21, 0, 0, 3, 0
+ spritePiece -$12, 4, 1, 1, $16, 0, 0, 3, 0
+ spritePiece -$A, 4, 1, 1, $1C, 0, 0, 3, 0
+ spritePiece -1, 4, 1, 1, $1B, 0, 0, 3, 0
+ spritePiece 7, 4, 1, 1, $20, 0, 0, 3, 0
+Map_obj0F_0006_Clone_End
 
 	even
