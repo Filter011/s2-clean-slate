a2a8d1a1fd2fb40a54e830a33bbf0b88de6ef9b9
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2026-09-05 20:07:21 -0600

    add convsym

--- build.lua
+++ build.lua
@@ -20,6 +20,13 @@ common.convert_dpcm_files_in_directory("sound/DAC")
 
 -- Build the ROM.
 common.build_rom_and_handle_failure("s2", "s2built", "", "-p=0 -z=0," .. "kosinskiplus" .. ",Size_of_Snd_driver_guess,after", true, repository)
+-- Append debug symbols to ROMs using ConvSym
+local extra_tools = common.find_tools("debug symbol generator", "https://github.com/vladikcomper/md-modules", repository, "convsym")
+if extra_tools == nil then
+	common.show_flashy_message("Build failed. See above for more details.")
+	os.exit(false)
+	end
+	os.execute(extra_tools.convsym .. " s2.lst s2built.bin -input as_lst -range 0 FFFFFF -a")
 
 -- Remove the header file, since we no longer need it.
 os.remove("s2.h")
