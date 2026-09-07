98b95731e793d5c2e7fb77177113847026041e35
Filter <not.so.filter@hotmail.com>
2026-09-07 16:22:24 -0400

    fix issue when cfSetCommunication is in use

--- s2.sounddriver.asm
+++ s2.sounddriver.asm
@@ -2519,8 +2519,6 @@ cfPanningAMSFMS:
 ; zloc_D1A cfAlterNotesUNK cfAlterNotes
 cfDetune:
 	ld	(ix+zTrack.Detune),a	; set new detune value
-
-cfSetCommunication:
 	ret
 ; ---------------------------------------------------------------------------
 
@@ -2632,6 +2630,7 @@ cfPreventAttack:
 	set	4,(ix+zTrack.PlaybackControl)	; Set bit 4 (10h) on playback control; do not attack next note
 
 ; Unused commands
+cfSetCommunication:
 cfClearPush:
 cfStopSpecialFM4:
 	dec	hl				; Takes no argument, so just put it back
